9f42d04a87e3002497c8e4ef95f9925b60d42932
linhanjen <linhanjen@gmail.com>
2013-09-23 14:34:16 +0800

    start

---
+++ lib/hadoop/samtools.rb
@@ -0,0 +1,7 @@
+require "hadoop/samtools/version"
+
+module Hadoop
+  module Samtools
+    # Your code goes here...
+  end
+end
---
+++ lib/hadoop/samtools/version.rb
@@ -0,0 +1,5 @@
+module Hadoop
+  module Samtools
+    VERSION = "0.0.1"
+  end
+end
